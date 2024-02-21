868db55ee5a3b348558026888c4f98b0f14608f0
Solid Wires <solid.wires@gmail.com>
2024-02-21 12:02:59 -0500

    num threads defined

--- src/from-lecture/num-threads-defined/main.cpp
+++ src/from-lecture/num-threads-defined/main.cpp
@@ -1,6 +1,21 @@
 #include "../../../inc/Common.h"
 using namespace std;
 
+void Foo(int, double[1000]);
+
 int main() {
-    cout << "This is num threads defined" << endl;
+    double A[1000];
+    omp_set_num_threads(4);
+
+    #pragma omp parallel
+    {
+        int id = omp_get_thread_num();
+        Foo(id, A);
+    }
+    cout << "All done" << endl;
+}
+
+void Foo(int myId, double myA[1000]) {
+    cout << "thread: " << myId << endl;
+    return;
 }
