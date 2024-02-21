e97e34f1ad1dab9abeb973cdb0d7ed0b01e51efc
Solid Wires <solid.wires@gmail.com>
2024-02-21 11:48:41 -0500

    what about this

--- src/from-lecture/hello/main.cpp
+++ src/from-lecture/hello/main.cpp
@@ -5,7 +5,7 @@ int main() {
 
     #pragma omp parallel
     {
-        int id = omp_get_thread_num();
+        int id = omp_get_num_threads();
         cout << "Hello(" << id << ")";
         cout << "World(" << id << ")" << endl;
     }
