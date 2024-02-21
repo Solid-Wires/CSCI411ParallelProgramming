63f34e14f46b7e4224c2aa815467118c942446fa
Solid Wires <solid.wires@gmail.com>
2024-02-21 11:38:39 -0500

    set up these

---
+++ src/from-lecture/num-threads-defined/main.cpp
@@ -0,0 +1,6 @@
+#include "../../../inc/Common.h"
+using namespace std;
+
+int main() {
+    cout << "This is num threads defined" << endl;
+}
---
+++ src/from-lecture/pi-program/main.cpp
@@ -0,0 +1,6 @@
+#include "../../../inc/Common.h"
+using namespace std;
+
+int main() {
+    cout << "This is the pi program" << endl;
+}
