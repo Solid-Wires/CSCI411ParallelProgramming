b06ea0d4e0746fa63c27e141b033d73c3783cad1
Solid Wires <solid.wires@gmail.com>
2024-02-21 11:58:19 -0500

    track run time

--- src/from-lecture/num-integ/main.cpp
+++ src/from-lecture/num-integ/main.cpp
@@ -6,14 +6,16 @@ int main() {
     double step, x, pi, sum = 0.0;
     double start_time, run_time;
     step = 1.0/(double)num_steps;
+    start_time = omp_get_wtime();
 
     for (int i = 1; i <= num_steps; i++) {
         x = (i-0.5)*step;
         sum = sum + 4.0/(1.0+(x*x));
     }
     pi = step * sum;
+    run_time = omp_get_wtime() - start_time;
 
-    cout << " pi with " << num_steps << " steps is " << pi << endl;
+    cout << "pi with " << num_steps << " steps is " << pi << "" in  << run_time << " seconds" << endl;
 
     return 0;
 }
