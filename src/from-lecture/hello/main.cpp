68597848e1e9c84999c46473568c391180f4c464
Solid Wires <solid.wires@gmail.com>
2024-02-21 11:56:40 -0500

    Numerical integration

--- src/from-lecture/hello/main.cpp
+++ src/from-lecture/hello/main.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 int main() {
 
-    omp_set_num_threads(4);
     #pragma omp parallel
     {
         int id = omp_get_thread_num();
--- src/from-lecture/num-integ/main.cpp
+++ src/from-lecture/num-integ/main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
-    cout << "This is num integ" << endl;
+    long num_steps = 1000000000;
+    double step, x, pi, sum = 0.0;
+    double start_time, run_time;
+    step = 1.0/(double)num_steps;
+
+    for (int i = 1; i <= num_steps; i++) {
+        x = (i-0.5)*step;
+        sum = sum + 4.0/(1.0+(x*x));
+    }
+    pi = step * sum;
+
+    cout << " pi with " << num_steps << " steps is " << pi << endl;
+
+    return 0;
 }
