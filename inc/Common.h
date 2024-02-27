c78d3a0acfefa6d1b90e15a5ac613c6d676e7a46
Solid-Wires <solid.wires@gmail.com>
2024-02-27 02:24:45 -0500

    Begin working on threads project

--- inc/Common.h
+++ inc/Common.h
@@ -1,5 +1,8 @@
 # pragma once
 
+// Just has common includes
 #include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 #include <omp.h>
-
--- src/Main.cpp
+++
@@ -1,6 +0,0 @@
-#include "../inc/Common.h"
-using namespace std;
-
-int main() {
-    cout << "Tis is the main binary" << endl;
-}
