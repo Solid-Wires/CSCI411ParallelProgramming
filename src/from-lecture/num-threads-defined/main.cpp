14e44608b8e7785d1732e32d214b5651323e1b3b
Solid-Wires <solid.wires@gmail.com>
2024-02-27 00:53:16 -0500

    Lets try this

--- src/from-lecture/num-threads-defined/main.cpp
+++ src/from-lecture/num-threads-defined/main.cpp
@@ -6,7 +6,9 @@ void Foo(int, double[1000]);
 int main() {
     double A[1000];
     
-    #pragma omp parallel num_threads(4)
+    omp_set_dynamic(0);
+    omp_set_num_threads(4);
+    #pragma omp parallel
     {
         int id = omp_get_thread_num();
         Foo(id, A);
